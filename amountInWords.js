0682e5a6be8e660f307b06e21b24e94e1c825b7e
Roshan Kumar <53408220+roshan9419@users.noreply.github.com>
2021-09-25 14:45:47 +0530

    used preffered operators + camel case change

--- amountInWords.js
+++ amountInWords.js
@@ -1,15 +1,17 @@
 function inWords(num) {
-    const a = ['', 'one ', 'two ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ', 'nine ', 'ten ', 'eleven ', 'twelve ', 'thirteen ', 'fourteen ', 'fifteen ', 'sixteen ', 'seventeen ', 'eighteen ', 'nineteen '];
-    const b = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const a = ['', 'One ', 'Two ', 'Three ', 'Four ', 'Five ', 'Six ', 'Seven ', 'Eight ', 'Nine ', 'Ten ', 'Eleven ', 'Twelve ', 'Thirteen ', 'Fourteen ', 'Fifteen ', 'Sixteen ', 'Seventeen ', 'Eighteen ', 'Nineteen '];
+    const b = ['', '', 'Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety'];
 
     if ((num = num.toString()).length > 9) return '-';
     const n = ('000000000' + num).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
-    if (!n) return; var str = '';
-    str += (n[1] != 0) ? (a[Number(n[1])] || b[n[1][0]] + ' ' + a[n[1][1]]) + 'crore ' : '';
-    str += (n[2] != 0) ? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) + 'lakh ' : '';
-    str += (n[3] != 0) ? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) + 'thousand ' : '';
-    str += (n[4] != 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + 'hundred ' : '';
-    str += (n[5] != 0) ? '' + (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) : '';
+
+    if (!n) return '-';
+    let str = '';
+    str += (Number(n[1]) !== 0) ? (a[Number(n[1])] || b[n[1][0]] + ' ' + a[n[1][1]]) + 'Crore ' : '';
+    str += (Number(n[2]) !== 0) ? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) + 'Lakh ' : '';
+    str += (Number(n[3]) !== 0) ? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) + 'Thousand ' : '';
+    str += (Number(n[4]) !== 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + 'Hundred ' : '';
+    str += (Number(n[5]) !== 0) ? (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) : '';
     return str;
 }
 
@@ -17,13 +19,13 @@ function getAmountInWords(amount) {
     const beforeDecimal = parseInt(amount);
     const afterDecimal = parseInt((amount - beforeDecimal) * 100);
 
-    let finalWords = inWords(beforeDecimal) + "rupees ";
+    let finalWords = inWords(beforeDecimal) + "Rupees ";
     if (afterDecimal !== 0) {
-        finalWords += "and " + inWords(afterDecimal) + "paisa ";
+        finalWords += "And " + inWords(afterDecimal) + "Paisa ";
     }
-    finalWords += "only";
+    finalWords += "Only";
 
-    return finalWords.toUpperCase();
+    return finalWords;
 }
 
-console.log(getAmountInWords(1234.56)); // ONE THOUSAND TWO HUNDRED THIRTY FOUR RUPEES AND FIFTY SIX PAISA ONLY
+console.log(getAmountInWords(1234.56)); // One Thousand Two Hundred Thirty Four Rupees And Fifty Six Paisa Only
